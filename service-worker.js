a342a4ddaad2124aa0554faef330e7f83289b9d5
TomDotCom4 <77259800+TomDotCom4@users.noreply.github.com>
2023-06-29 17:55:07 +1000

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('my-pwa-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName.startsWith('my-pwa-') && cacheName !== 'my-pwa-cache';
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
