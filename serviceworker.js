ff3e8d1efb4543e7fa911ec608511dd3bae1af03
TomDotCom4 <77259800+TomDotCom4@users.noreply.github.com>
2023-08-15 23:02:24 +1000

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,33 +1,39 @@
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+  '/',
+  'manifest.json',
+  'icon/newproject.png',
+  'index.js'
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('my-pwa-cache').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/index.js',
-      ]);
-    })
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(cacheName => {
-          return cacheName.startsWith('my-pwa-') && cacheName !== 'my-pwa-cache';
-        }).map(cacheName => {
-          return caches.delete(cacheName);
-        })
-      );
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
